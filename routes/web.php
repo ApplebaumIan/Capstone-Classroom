51aa919b2720d1110c3bded26b53d31d04f0195e
Ian Applebaum <Ian.tyler@electricbluefish.com>
2026-09-13 14:18:36 -0400

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Socialite;
+
+Route::inertia('/', 'welcome')->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+});
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('github')->user();
+
+    // $user->token
+});
+
+require __DIR__.'/settings.php';
