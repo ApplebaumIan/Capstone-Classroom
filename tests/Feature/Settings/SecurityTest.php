3d93e6917579857c8348f16fccaf2448c31700f1
Ian Applebaum <Ian.tyler@electricbluefish.com>
2026-09-17 21:12:07 -0400

    Removed more fortify code.

--- tests/Feature/Settings/SecurityTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-test('password and two factor settings are unavailable', function () {
-    expect(Route::has('security.edit'))->toBeFalse()
-        ->and(Route::has('user-password.update'))->toBeFalse()
-        ->and(config('fortify.features'))->toBe([]);
-});
